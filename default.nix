dd766ad465ce0947f3816d2ec64f3111f05f21a1
qwerty <temp@qwerty.qq>
2025-08-24 20:37:39 +0300

    best default.nix

--- default.nix
+++ default.nix
@@ -16,33 +16,31 @@ stdenv.mkDerivation {
   version = "unstable-${self.shortRev or "dirty"}";
   src = self;
 
-  nativeBuildInputs = [wrapGAppsHook3 pkg-config makeWrapper gtk3];
-
-  buildInputs = [ax-shell-python tabler-icons-font] ++ runtimeDeps;
+  nativeBuildInputs = [ wrapGAppsHook3 pkg-config makeWrapper gtk3 ];
+  buildInputs = [ ax-shell-python tabler-icons-font ] ++ runtimeDeps;
   dontWrapQtApps = true;
 
+  pythonPath = [ self ];
+
   installPhase = ''
-    runHook preInstall
-    mkdir -p $out/lib/ax-shell
-    cp -r ./* $out/lib/ax-shell/
+    runHook preInstall;
 
+    mkdir -p $out/bin;
     makeWrapper ${ax-shell-python}/bin/python $out/bin/ax-shell \
-      --add-flags "$out/lib/ax-shell/main.py"
+      --add-flags "-m main"
 
-    runHook postInstall
+    runHook postInstall;
   '';
 
   preFixup = ''
-    gappsWrapperArgs+=(--prefix PYTHONPATH : "$out/lib/ax-shell")
-    gappsWrapperArgs+=(--set FABRIC_CSS_PATH "$out/lib/ax-shell/main.css")
-    gappsWrapperArgs+=(--prefix PATH : "${lib.makeBinPath runtimeDeps}")
-    gappsWrapperArgs+=(--set XCURSOR_THEME "Adwaita")
-    gappsWrapperArgs+=(--set XCURSOR_SIZE "24")
-    gappsWrapperArgs+=(--prefix XCURSOR_PATH : "${adwaita-icon-theme}/share/icons")
-    gappsWrapperArgs+=(--prefix XDG_DATA_DIRS : "${tabler-icons-font}/share")
-    gappsWrapperArgs+=(--prefix XDG_DATA_DIRS : "${adwaita-icon-theme}/share")
+    gappsWrapperArgs+=(--set FABRIC_CSS_PATH "${self}/main.css");
+    gappsWrapperArgs+=(--prefix PATH : "${lib.makeBinPath runtimeDeps}");
+    gappsWrapperArgs+=(--set XCURSOR_THEME "Adwaita");
+    gappsWrapperArgs+=(--set XCURSOR_SIZE "24");
+    gappsWrapperArgs+=(--prefix XCURSOR_PATH : "${adwaita-icon-theme}/share/icons");
+    gappsWrapperArgs+=(--prefix XDG_DATA_DIRS : "${tabler-icons-font}/share");
+    gappsWrapperArgs+=(--prefix XDG_DATA_DIRS : "${adwaita-icon-theme}/share");
   '';
-
 
   meta = {
     description = "A custom, flake-based package for Ax-Shell.";
