c24d08e990c78d8d614c06ee469dd8355ddd9159
qwerty <temp@qwerty.qq>
2025-08-25 20:51:17 +0300

    added autostart for hm

--- nix/modules/home-manager.nix
+++ nix/modules/home-manager.nix
@@ -4,6 +4,7 @@ with lib;
 
 let
   cfg = config.programs.ax-shell;
+  hyprlandEnabled = config.wayland.windowManager.hyprland.enable;
 in
 {
   options.programs.ax-shell = {
@@ -13,11 +14,31 @@ in
       type = types.package;
       default = pkgs.ax-shell;
       defaultText = literalExpression "pkgs.ax-shell";
-      description = "Пакет Ax-Shell для установки.";
+      description = "Какой пакет Ax-Shell использовать.";
+    };
+
+    autostart = {
+      enable = mkEnableOption "автозапуск Ax-Shell вместе с Hyprland" // {
+        default = true;
+      };
+
+      logPath = mkOption {
+        type = types.str;
+        default = "${config.xdg.stateHome}/ax-shell/main.log";
+        description = "Путь к файлу для логирования Ax-Shell.";
+      };
     };
   };
 
-  config = mkIf cfg.enable {
-    home.packages = [ cfg.package ];
-  };
+  config = lib.mkMerge [
+    (mkIf cfg.enable {
+      home.packages = [ cfg.package ];
+    })
+
+    (mkIf (cfg.enable && cfg.autostart.enable && hyprlandEnabled) {
+      wayland.windowManager.hyprland.settings.exec-once = [
+        "${cfg.package}/bin/ax-shell &> ${cfg.autostart.logPath}"
+      ];
+    })
+  ];
 }
