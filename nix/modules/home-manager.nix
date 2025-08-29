062bdae12763eaecb4611175e2cb57b4536130ba
qwerty <temp@qwerty.qq>
2025-08-29 17:10:35 +0300

    test uwsm-app

--- nix/modules/home-manager.nix
+++ nix/modules/home-manager.nix
@@ -1,5 +1,3 @@
-# ~/.config/Ax-Shell/nix/modules/home-manager.nix
-
 { config, pkgs, lib, ... }:
 
 with lib;
@@ -8,15 +6,10 @@ let
   cfg = config.programs.ax-shell;
   hyprlandEnabled = config.wayland.windowManager.hyprland.enable;
 
-  # --- Генератор JSON ---
-  # Эта функция берет настройки из Nix и преобразует их в формат,
-  # который ожидает Python-скрипт (например, bar.centered -> centered_bar).
   formatJson = settings: {
     wallpapers_dir = settings.wallpapersDir;
     terminal_command = settings.terminalCommand;
     datetime_12h_format = settings.datetime12hFormat;
-
-    # Секция "bar"
     bar_position = settings.bar.position;
     centered_bar = settings.bar.centered;
     bar_theme = settings.bar.theme;
@@ -24,8 +17,6 @@ let
     bar_workspace_use_chinese_numerals = settings.bar.workspace.useChineseNumerals;
     bar_hide_special_workspace = settings.bar.workspace.hideSpecial;
     bar_metrics_disks = settings.bar.metrics.disks;
-
-    # Видимость компонентов бара
     bar_button_apps_visible = settings.bar.components.appsButton;
     bar_systray_visible = settings.bar.components.systray;
     bar_control_visible = settings.bar.components.control;
@@ -40,25 +31,19 @@ let
     bar_language_visible = settings.bar.components.language;
     bar_date_time_visible = settings.bar.components.dateTime;
     bar_button_power_visible = settings.bar.components.powerButton;
-
-    # Секция "dock"
     dock_enabled = settings.dock.enable;
     dock_always_occluded = settings.dock.alwaysOccluded;
     dock_icon_size = settings.dock.iconSize;
     dock_theme = settings.dock.theme;
-
-    # Секция "panel"
     panel_theme = settings.panel.theme;
     panel_position = settings.panel.position;
-
-    # Остальные настройки
     notif_pos = settings.notifications.position;
     metrics_visible = settings.metrics.main;
     metrics_small_visible = settings.metrics.small;
   };
 
-in {
-  # --- Опции, которые пользователь сможет настраивать ---
+in
+{
   options.programs.ax-shell = {
     enable = mkEnableOption "Ax-Shell";
 
@@ -80,57 +65,48 @@ in {
       };
     };
 
-    # --- НОВАЯ СЕКЦИЯ: Декларативная конфигурация Ax-Shell ---
     settings = {
       wallpapersDir = mkOption {
         type = types.str;
         default = "${cfg.package}/share/ax-shell/assets/wallpapers_example";
         description = "Путь к директории с обоями.";
       };
-
       defaultWallpaper = mkOption {
-        type = types.path; # Тип "путь", Nix проверит, что файл существует
+        type = types.path;
         default = "${cfg.package}/share/ax-shell/assets/wallpapers_example/example-1.jpg";
         description = "Путь к изображению, которое будет использоваться как обои по умолчанию.";
       };
-
       terminalCommand = mkOption {
         type = types.str;
         default = "kitty -e";
         description = "Команда для запуска терминала.";
       };
-      
       datetime12hFormat = mkOption {
         type = types.bool;
         default = false;
         description = "Использовать 12-часовой формат времени.";
       };
-
       bar = {
         position = mkOption {
           type = types.enum [ "Top" "Bottom" "Left" "Right" ];
           default = "Top";
           description = "Позиция основной панели.";
         };
-
         centered = mkOption {
           type = types.bool;
           default = false;
           description = "Центрировать ли компоненты на панели.";
         };
-
         theme = mkOption {
-          type = types.str; # Можно будет сделать enum
+          type = types.str;
           default = "Pills";
           description = "Тема оформления панели.";
         };
-        
         workspace = {
-            showNumber = mkOption { type = types.bool; default = false; };
-            useChineseNumerals = mkOption { type = types.bool; default = false; };
-            hideSpecial = mkOption { type = types.bool; default = true; };
+          showNumber = mkOption { type = types.bool; default = false; };
+          useChineseNumerals = mkOption { type = types.bool; default = false; };
+          hideSpecial = mkOption { type = types.bool; default = true; };
         };
-
         metrics = {
           disks = mkOption {
             type = types.listOf types.str;
@@ -138,31 +114,24 @@ in {
             description = "Список дисков для отображения в виджете метрик.";
           };
         };
-
         components = mkEnableOption "видимость компонентов на панели" // {
-          # Используем `mkOptionDefault` для установки всех значений по умолчанию в `true`
           default = mapAttrs (_: _: true) (import ./component-types.nix);
-          # Тип - это атрибут-сет булевых значений
           type = with types; attrsOf bool;
         };
       };
-
       dock = {
         enable = mkOption { type = types.bool; default = true; };
         alwaysOccluded = mkOption { type = types.bool; default = false; };
         iconSize = mkOption { type = types.int; default = 28; };
         theme = mkOption { type = types.str; default = "Pills"; };
       };
-
       panel = {
         theme = mkOption { type = types.str; default = "Notch"; };
         position = mkOption { type = types.str; default = "Center"; };
       };
-      
       notifications = {
-          position = mkOption { type = types.enum [ "Top" "Bottom" ]; default = "Top"; };
+        position = mkOption { type = types.enum [ "Top" "Bottom" ]; default = "Top"; };
       };
-
       metrics = {
         main = mkOption {
           type = with types; attrsOf bool;
@@ -176,39 +145,49 @@ in {
     };
   };
 
-  # --- Конфигурация, которая будет применяться ---
-  config = mkIf cfg.enable (let
-    # 1. Генерируем JSON из настроек пользователя
-    jsonConfigFile = pkgs.writeText "ax-shell-config.json" (builtins.toJSON (formatJson cfg.settings));
+  config = mkIf cfg.enable (
+    let
+      wrappedPackage = pkgs.symlinkJoin {
+        name = "ax-shell-with-declarative-config";
+        paths = [ cfg.package ];
+        nativeBuildInputs = [ pkgs.makeWrapper ];
+        postBuild = ''
+          wrapProgram $out/bin/ax-shell \
+            --set AX_SHELL_CONFIG_FILE "${jsonConfigFile}"
+        '';
+      };
 
-    # 2. Создаем "обернутый" пакет, который знает о нашем конфиге
-    wrappedPackage = pkgs.symlinkJoin {
-      name = "ax-shell-with-declarative-config";
-      paths = [ cfg.package ]; # Берем оригинальный пакет
-      nativeBuildInputs = [ pkgs.makeWrapper ];
-      postBuild = ''
-        # "Оборачиваем" бинарник, добавляя переменную окружения
-        wrapProgram $out/bin/ax-shell \
-          --set AX_SHELL_CONFIG_FILE "${jsonConfigFile}"
-      '';
-    };
-  in {
-    home.packages = [ 
-      wrappedPackage
-      pkgs.swww
-      pkgs.matugen
-    ];
+      jsonConfigFile = pkgs.writeText "ax-shell-config.json" (builtins.toJSON (formatJson cfg.settings));
 
-    home.file."${config.xdg.configHome}/ax-shell/current.wall" = {
-      source = cfg.settings.defaultWallpaper;
-      force = true;
-    };
-
-    wayland.windowManager.hyprland.settings = mkIf (cfg.autostart.enable && hyprlandEnabled) {
-      exec-once = [
-        "uwsm app -- swww-daemon"
-        "uwsm-app $(${wrappedPackage}/bin/ax-shell &> ${cfg.autostart.logPath})"
+    in
+    {
+      home.packages = [
+        wrappedPackage
+        pkgs.uwsm
+        pkgs.swww
+        pkgs.matugen
       ];
-    };
-  });
+
+      home.file."${config.xdg.configHome}/ax-shell/current.wall" = {
+        source = cfg.settings.defaultWallpaper;
+        force = true;
+      };
+
+      wayland.windowManager.hyprland.settings = mkIf (hyprlandEnabled) (
+        let
+          uwsm-app = "${pkgs.uwsm}/bin/uwsm-app";
+          swww-daemon = "${pkgs.swww}/bin/swww-daemon";
+          ax-shell-run = ''
+            ${pkgs.bash}/bin/bash -c "${wrappedPackage}/bin/ax-shell &> ${cfg.autostart.logPath}"
+          '';
+        in
+        {
+          exec-once = mkIf cfg.autostart.enable [
+            "${uwsm-app} -- ${swww-daemon}"
+            "${uwsm-app} -- ${ax-shell-run}"
+          ];
+        }
+      );
+    }
+  );
 }
